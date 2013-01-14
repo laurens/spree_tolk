1bd8c8d81262bc1c40514853ea15ef862c6446d0
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-01-14 13:55:40 +0100

    Styles and JS enhancement

--- app/assets/javascripts/admin/spree_tolk.js
+++ app/assets/javascripts/admin/spree_tolk.js
@@ -1,1 +1,15 @@
 //= require admin/spree_core
+
+$(function() {
+  var container = $("div.tolk_translations");
+  
+  if (!container.length) {
+    return;
+  }
+
+  container.find("td textarea").each(function() {
+    var textarea = $(this);
+    
+    textarea.css('height', textarea.closest("td").height());
+  })
+})
