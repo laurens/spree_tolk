cca70005c447eb97190004b04f8cac7de68a2e93
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-01-14 14:04:38 +0100

    Fix Tab

--- app/overrides/admin/tabs.rb
+++ app/overrides/admin/tabs.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "add tolk tab",
                      :insert_bottom => "[data-hook='admin_tabs'], #admin_tabs[data-hook]",
-                     :text => "<%= tab(:locales, :url => spree.tolk_path, :label => :translations, :match_path => '/tolk' ) %>",
+                     :text => "<%= tab(:locales, :searches, :url => spree.tolk_path ) %>",
                      :disabled => false)
