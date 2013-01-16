88feb8ffd59a0604c9ca48a56e20f479ba5b353e
Laurens Nienhaus <l.nienhaus@gmail.com>
2013-01-16 23:08:57 +0100

    Revert "Move Locale language_name attribute to database"
    Intended use case was using tolk together with globalize3.
    But both define an association named :translations.
    
    This reverts commit bf9888282eab32f11178ee555eaa0f919dad7083.

--- app/models/tolk/locale.rb
+++ app/models/tolk/locale.rb
@@ -15,7 +15,7 @@ module Tolk
     accepts_nested_attributes_for :translations, :reject_if => proc { |attributes| attributes['text'].blank? }
     before_validation :remove_invalid_translations_from_target, :on => :update
 
-    attr_accessible :name, :language_name
+    attr_accessible :name
     cattr_accessor :locales_config_path
     self.locales_config_path = self._dump_path
 
@@ -159,7 +159,7 @@ module Tolk
     end
 
     def language_name
-      read_attribute(:language_name) || self.name
+      Tolk.config.mapping[self.name] || self.name
     end
 
     def get(key)
